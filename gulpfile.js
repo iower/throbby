2eaa404a0ccc57f10af6fca29a9844c4d2efcc3a
iower <iower@tutanota.com>
2018-08-07 11:52:32 +0500

    Rewrite the project to SASS

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+	browserSync = require('browser-sync'),
+	sass = require('gulp-sass'),
+	rename = require('gulp-rename'),
+	notify = require('gulp-notify');
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', ['sassToCss', 'browserSyncTask'], function() {
+	gulp.watch('*.sass', ['sassToCss']);
+});
+
+gulp.task('browserSyncTask', function() {
+	browserSync({
+		proxy: 'localhost:8080',
+	});
+});
+
+gulp.task('sassToCss', function() {
+	return gulp.src('*.sass')
+		.pipe(sass({outputStule: 'expand'}).on('error', notify.onError()))
+		//.pipe(rename({suffix: '.min', prefix: ''}))
+		.pipe(gulp.dest('./'))
+		.pipe(browserSync.stream())
+});
